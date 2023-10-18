fe7c4cf74cd9e28e0f96ac6d525c0e404ff6cd92
Harr-ux <110978468+Harr-ux@users.noreply.github.com>
2023-10-18 20:02:17 +0000

    Math

---
+++ 01_basics/09_NumsandMath.js
@@ -0,0 +1,20 @@
+const score = 800
+// console.log(score)
+const balance = new Number(100)
+console.log(balance);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.4567;
+// console.log(otherNumber.toPrecision(3));
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+// +++++++Maths+++++ // 
+console.log(Math);
+console.log(Math.abs(-3));
+console.log(Math.round(3.4));
+console.log(Math.min(2,34,4,5));
+console.log(Math.random());
+const min = 10
+const max = 20
+console.log(Math.random() * (max-min + 1) + min)
+
