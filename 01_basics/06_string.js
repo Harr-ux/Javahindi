28b688ffcaa055068857bb4ca0eb2ae312f8c639
Harr-ux <110978468+Harr-ux@users.noreply.github.com>
2023-10-18 19:33:34 +0000

    Done

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -1,3 +1,18 @@
 let val = 3;
 let negval = -val
 console.log(negval);
+const getname = new String('Hitesh')
+console.log(getname)
+console.log(getname.__proto__ );
+console.log(getname.length)
+console.log(getname.charAt(2))
+const newstring = getname.substring(0,4);
+console.log(newstring)
+const anewstring = getname.slice(0,4);
+console.log(anewstring);
+
+const anotherstring = " aHJ  "
+console.log(anotherstring)
+console.log(anotherstring.trim());
+
+
---
+++ 01_basics/08_datatype.js
@@ -0,0 +1,11 @@
+let myYoutube = "Harshit"
+let anothername = "Chaha";
+console.log(myYoutube);
+console.log(anothername);
+let userOne = {
+    email:"user@ybl",
+    upi : "user@paytm",
+    Name : "Harshit",
+
+}
+console.log(userOne.email);
