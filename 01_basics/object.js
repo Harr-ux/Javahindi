dbec2ebb909c07bb6299886f443364adf20f1cce
Harr-ux <110978468+Harr-ux@users.noreply.github.com>
2023-11-03 18:25:18 +0000

    object

---
+++ 01_basics/object.js
@@ -0,0 +1,9 @@
+const jsuser = {
+    name : "Hitesh",
+    age : 18,
+    location:"jaipur",
+    email:"harshitjoshi@gmail.com",
+    isloggedin: false,
+}
+console.log(jsuser.email)
+console.log(jsuser[email])
