0d45f212d5621facea7126523843eade027c64f5
Harr-ux <110978468+Harr-ux@users.noreply.github.com>
2023-11-05 16:49:29 +0000

    Object

--- 01_basics/object.js
+++ 01_basics/object.js
@@ -1,9 +1,16 @@
-const jsuser = {
-    name : "Hitesh",
-    age : 18,
-    location:"jaipur",
-    email:"harshitjoshi@gmail.com",
-    isloggedin: false,
+// const jsuser = {
+//     name : "Hitesh",
+//     age : 18,
+//     location:"jaipur",
+//     email:"harshitjoshi@gmail.com",
+//     isloggedin: false,
+// }
+// console.log(jsuser.email)
+// console.log(jsuser[email])
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseinstructor : "harshit",
 }
-console.log(jsuser.email)
-console.log(jsuser[email])
+const {courseinstructor} = course
+console.log(courseinstructor);
