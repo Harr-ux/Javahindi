fdb28a4416040a8dbccd779e92c254876460051b
Harr-ux <110978468+Harr-ux@users.noreply.github.com>
2023-11-08 18:33:11 +0000

    Scopelevel

---
+++ 01_basics/scopelevel.js
@@ -0,0 +1,21 @@
+function one (){
+    const username = "Harshit"
+    function two (){
+        const website = "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+two ();
+}
+ one();
+  
+ // Declaration Of Functions // 
+ function addone(num) {
+    return num+1;
+ }
+ addone (6);
+
+ const addtwo = function addtwo (num){
+    return  num+2;
+ }
+ addtwo(7);
